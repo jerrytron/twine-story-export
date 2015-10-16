036abf9dbfc7e3be2f52873ad7a7e99c95073baf
jerrytron <jerry@monkeytheater.com>
2015-10-16 17:54:30 -0500

    Working on random linear output. Not QUITE there.

--- cdam_convert_twine.py
+++ cdam_convert_twine.py
@@ -4,6 +4,8 @@ import os
 import re
 import sys
 import struct
+import pprint
+import random
 import argparse
 import tiddlywiki as tiddly
 import cdam_gen_files as gen
@@ -30,6 +32,8 @@ OPERATION_TEST = bytearray()
 TOTAL_OPS = 0
 VERBOSE = False
 LINEAR = False
+
+PP = pprint.PrettyPrinter(indent = 4)
 
 kAppend = "<append>"
 kContinue = "<continue>"
@@ -149,48 +153,43 @@ def main():
 
 		bookPath = STORY_TITLE.lower().replace(" ", "_") + "_LINEAR.txt"
 		bookPath = os.path.join(args.output, bookPath)
-		goto = " (go to "
 
 		book = "Title: " + STORY_TITLE + "\nSubtitle: " + STORY_SUBTITLE + "\nAuthor: " + STORY_AUTHOR
 		book += "\nCredits: " + STORY_CREDITS + "\nContact: " + STORY_CONTACT + "\nLanguage: " + STORY_LANGUAGE + "\nVersion: " + STORY_VERSION + "\n\n\n"
 
-		p = PASSAGES['0']
-		del PASSAGES['0']
-		m = STORY_MAP['0']
-		del STORY_MAP['0']
-		book += "[0] " + p['pt']
-		# Add a delimeter so we know it is done
-		book += "\n---"
-		for index in range(0, len(p['cs'])):
-			book += ("\n- " + p['cs'][index] + goto + m[index] + ")")
+		ordering = []
 
-		for index in range(1, len(PASSAGES) + 1):
-			key = str(index)
-			book += "\n\n\n"
-			p = PASSAGES[key]
-			m = STORY_MAP[key]
-			book += "[" + key + "] " + p['pt']
-			# Add a delimeter so we know it is done
-			book += "\n---"
+		key = "0"
+		p = PASSAGES[key]
+		book += linearPassageText(PASSAGES, STORY_MAP, key)
 
-			if p['en'] == True:
-				if p['eq'] == 1:
-					book += "\n* - THE END"#* Oh no! Better luck next adventure. * - THE END"
-				elif p['eq'] == 2:
-					book += "\n** - THE END"#** I'm sure you can do better. ** - THE END"
-				elif p['eq'] == 3:
-					book += "\n*** - THE END"#*** You win some, you lose some. *** - THE END"
-				elif p['eq'] == 4:
-					book += "\n**** - THE END"#**** Not too bad! **** - THE END"
-				elif p['eq'] == 5:
-					book += "\n***** - THE END"#***** Congratulations! You sure know your stuff. ***** - THE END"
+		index = 1
+
+		while index < len(PASSAGES):
+			print key
+			if "ck" in p and len(p["ck"]) == 1:
+				p = PASSAGES[p["ck"][0]]
+				#del PASSAGES[key]
+				key = p["key"]
+				book += linearPassageText(PASSAGES, STORY_MAP, key)
+				#del PASSAGES[p["key"]]
 			else:
-				if len(p['cs']) == 1:
-					book += ("\n- " + p['cs'][0] + goto + m[0] + ")")
-				else:
-					for index in range(0, len(p['cs'])):
-						book += ("\n- " + p['cs'][index] + goto + m[index] + ")")
+				#del PASSAGES[key]
+				key = random.choice(list(PASSAGES.keys()))
+				p = PASSAGES[key]
+				key = p["key"]
+				book += linearPassageText(PASSAGES, STORY_MAP, key)
+			if len(PASSAGES) != 0:
+				book += "\n\n\n"
+			index += 1
 
+		#for index in range(1, len(PASSAGES) + 1):
+		#	p = PASSAGES[str(index)]
+		#	book += linearPassageText(PASSAGES, STORY_MAP, p["key"])
+
+		#PP.pprint(PASSAGES)
+		#print "\n"
+		#PP.pprint(STORY_MAP)
 		if os.path.exists(bookPath):
 			os.remove(bookPath)
 		genFile.WriteToFile(bookPath, book)
@@ -210,6 +209,34 @@ def main():
 	print "Complete!"
 	#print STORY_MAP
 	#print PASSAGES
+
+def linearPassageText(aPassages, aStoryMap, aKey):
+	psgText = ""
+	goto = " (go to "
+	p = aPassages[aKey]
+	m = aStoryMap[aKey]
+	psgText += "[" + aKey + "] " + p['pt']
+	# Add a delimeter so we know it is done
+	psgText += "\n---"
+
+	if p['en'] == True:
+		if p['eq'] == 1:
+			psgText += "\n* - THE END"#* Oh no! Better luck next adventure. * - THE END"
+		elif p['eq'] == 2:
+			psgText += "\n** - THE END"#** I'm sure you can do better. ** - THE END"
+		elif p['eq'] == 3:
+			psgText += "\n*** - THE END"#*** You win some, you lose some. *** - THE END"
+		elif p['eq'] == 4:
+			psgText += "\n**** - THE END"#**** Not too bad! **** - THE END"
+		elif p['eq'] == 5:
+			psgText += "\n***** - THE END"#***** Congratulations! You sure know your stuff. ***** - THE END"
+	else:
+		if len(p['cs']) == 1:
+			psgText += ("\n- " + p['cs'][0] + goto + m[0] + ")")
+		else:
+			for index in range(0, len(p['cs'])):
+				psgText += ("\n- " + p['cs'][index] + goto + m[index] + ")")
+	return psgText
 
 def twineBuild(storySource, path, storyDir, title, author):
 	STORY_MAP.clear()
@@ -273,6 +300,7 @@ def BuildCDAMStory(wiki):
 		passage = ParseForAttributes(wiki.tiddlers[key].tags)
 		if passage == False:
 			continue
+		# Is this the starting passage?
 		if key.upper() == "START":
 			if "ps" not in passage:
 				passage["ps"] = 0
@@ -299,6 +327,7 @@ def BuildCDAMStory(wiki):
 			passage["pp"] = 0
 		rss = wiki.tiddlers[key].toRss()
 		choicePairs = ParseForChoices(rss.description)
+		#PP.pprint(choicePairs)
 		passage["pt"] = ParseForBody(rss.description)
 
 		if type(choicePairs) is bool:
@@ -323,7 +352,6 @@ def BuildCDAMStory(wiki):
 				for node in item:
 					nodes.append(node)
 					choices.append(item[node])
-
 			if ValidateChoices(wiki.tiddlers, nodes) == False:
 				print "[ERROR] Failed to validate choices for node."
 				return False
@@ -331,6 +359,7 @@ def BuildCDAMStory(wiki):
 				STORY_MAP[key.upper()] = nodes
 			passage["en"] = False
 			passage["cs"] = choices
+			passage["ck"] = nodes
 		#print "Validating passage for node " + key
 		if ValidatePassage(passage) == False:
 			print "[ERROR] Failed to validate passage."
@@ -658,6 +687,7 @@ def SimplifyNaming():
 				i += 1
 		STORY_MAP[TITLE_MAP[titleKey]] = newMap[titleKey]
 		PASSAGES[TITLE_MAP[titleKey]] = newPassages[titleKey]
+		PASSAGES[TITLE_MAP[titleKey]]['key'] = TITLE_MAP[titleKey]
 
 if __name__ == '__main__':
 	#global _UPDATE
