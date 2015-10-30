0a3c8092ba130b00a95165c315e8e040354eb214
jerrytron <jerry@monkeytheater.com>
2015-10-30 17:07:17 -0500

    Extra safety check. Hopefully fixes?

--- cdam_convert_twine.py
+++ cdam_convert_twine.py
@@ -183,13 +183,14 @@ def main():
 				key = random.choice(allKeys)
 				# If this passage has a single entrance, that passage should be
 				# put in first.
-				while len(PASSAGES[key]["ik"]) == 1:
-					# Keep tracing back until we find the first passage in a series
-					# of single paths, or until we hit a passage already used.
-					if PASSAGES[key]["ik"][0] in allKeys:
-						key = PASSAGES[key]["ik"][0]
-					else:
-						break
+				if "ik" in PASSAGES[key]["ik"]:
+					while len(PASSAGES[key]["ik"]) == 1:
+						# Keep tracing back until we find the first passage in a series
+						# of single paths, or until we hit a passage already used.
+						if PASSAGES[key]["ik"][0] in allKeys:
+							key = PASSAGES[key]["ik"][0]
+						else:
+							break
 				if key in allKeys:
 					allKeys.remove(key)
 				p = PASSAGES[key]
